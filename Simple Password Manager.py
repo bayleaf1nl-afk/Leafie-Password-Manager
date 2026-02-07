62d1bf335a2f3853d9c267380309989ce39368a8
bayleaf <bayleaf1nl@gmail.com>
2026-02-08 00:23:52 +0200

    added the basic function for the manager window.

--- Simple Password Manager.py
+++ Simple Password Manager.py
@@ -1,14 +1,28 @@
 import cryptography
 import json
+from PySide6.QtWidgets import *
 
 passwordExample = {
     "gmail": "bayleaf@gmail.john",
     "website": "https://www.bayleaf.com",
     "password": "IAMAVERYSECUREPASSWORDRAAAAAAAAAAAAAGH"
 }
+# TODO: make this appear iff it doesnt exist & encrypt it
+# with open("password.json", "w") as PassFile:
+#     data = json.dump(passwordExample, PassFile)
 
-with open("password.json", "w") as PassFile:
-    data = json.dump(passwordExample, PassFile)
+class MainWindow(QMainWindow): 
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("RAAAAAAAAAAAAAAGH")
+        button = QPushButton("water me")
+        self.setCentralWidget(button)
+    
 
-print(data)
-print(type(data))
+
+program = QApplication([])
+window = MainWindow()
+window.show()
+
+program.exec()
+
