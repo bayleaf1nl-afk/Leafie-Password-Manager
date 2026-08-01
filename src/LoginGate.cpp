1747143aec6a1d697ca21fcbc13582b183582e1a
bayleaf <bayleaf1nl@gmail.com>
2026-08-01 20:13:20 +0300

    1 - added [LoginGate.cpp]: implement master password persistence using QFile // need error handling review 2 - misc: added qfiledevice, qlogging, and qmessagebox includes

--- src/LoginGate.cpp
+++ src/LoginGate.cpp
@@ -2,11 +2,16 @@
 #include "GenericDialog.h"
 #include <QLineEdit>
 #include <QDialog>
+#include <qfiledevice.h>
 #include <qlineedit.h>
+#include <qlogging.h>
+#include <qmessagebox.h>
 #include <qwindowdefs.h>
+#include <QFile>
 
 bool LoginGate::authenticate(QString &outMasterPassword){
-    bool isFirstLaunch = true; //temp hardcoded
+    QFile file("master.txt");
+    bool isFirstLaunch = !file.exists();
     QString title = isFirstLaunch ? "Set Master Password" : "Login";
     QString label = isFirstLaunch ? "Make New Master Password:" : "Enter Master Password:";
     GenericDialog dialog(title, {{label, QLineEdit::Password}}, false, nullptr);
@@ -14,8 +19,13 @@ bool LoginGate::authenticate(QString &outMasterPassword){
     if (dialog.exec() != QDialog::Accepted) return false; //mf pressed the cancel button like a LOSER RAHHHH
     if (isFirstLaunch){
         outMasterPassword = dialog.inputText(0);
+        if (!file.open(QIODevice::WriteOnly)) { QMessageBox::critical(nullptr, "Error", "Could not write to master password file"); return false; }
+        
+        file.write(outMasterPassword.toUtf8());
         return true;
     }
-
-    return dialog.inputText(0) == outMasterPassword;
+    bool readFile = file.open(QIODevice::ReadOnly);
+    if (!file.open(QIODevice::ReadOnly)) { QMessageBox::critical(nullptr, "Error", "Could not read from master password file"); return false; }
+    QString stored = QString::fromUtf8(file.readAll());
+    return dialog.inputText(0) == stored;
 }
